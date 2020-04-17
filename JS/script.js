94da09941674e80dd50defc8ff9dd77c94ee6f22
Lamptey49 <lampteyphinehas70@gmail.com>
2020-04-17 01:36:24 +0100

    buid completed for github

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["I'm a Web Developer", "I'm Software Engineer", "I'm a full stack Developer"],
+        typeSpeed:100,
+        backSpeed:100,
+        fadeOut:false,
+        smartBackSpeed:true,
+        loop:true
+    });
+})(jQuery);
